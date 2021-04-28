702c8a014da8c9684f46667b0668c9c5977ae86b
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-29 00:21:08 +0200

    Aggiunto bonus sistemata formattazione

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-// chiedo all’utente il cognome
-
-var cognomeUtente = prompt("Inserisci il tuo cognome");
-cognomeUtente = cognomeUtente.toLowerCase().replace(/\b[a-z]/g, function(letter) { return letter.toUpperCase(); });
+// chiedo all’utente il cognome e mi accerto il campo non sia vuoto o numerico
+for (var i = 0; i = Infinity; i++) {
+  var cognomeUtente = prompt("Inserisci il tuo cognome");
+  cognomeUtente = cognomeUtente.toLowerCase().replace(/\b[a-z]/g, function(letter) { return letter.toUpperCase(); });
+if (isNaN(cognomeUtente)) { 
+    break; 
+  }else{
+    alert("Si prega di inserire correttamente il propio cognome")
+  }
+}
 
 // creo array 
 var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 listaCognomi.push(cognomeUtente);
 listaCognomi.sort();
-
 
 var i = 0;
 
@@ -15,14 +20,21 @@ while (i < listaCognomi.length){
   var elemento = listaCognomi[i];
   var contenuto = document.getElementById("lista_cognomi").innerHTML;
   document.getElementById("lista_cognomi").innerHTML = contenuto + "<li>" + elemento + "</li>";
-
- 
+  
     i++;
 }
 
 var numeroLista = listaCognomi.indexOf(cognomeUtente);
 
-document.getElementById("numero_coda").innerHTML = "Sei al numero " + (numeroLista + 1) + " della lista";
+document.getElementById("numero_coda").innerHTML = "Sei al " + (numeroLista + 1) + "° posto della lista";
+
+
+if (numeroLista == 0){
+  document.write("Tocca a te!");
+
+}else {
+  document.write("Altre " + numeroLista + " persone e tocca a te!");
+}
 
 
 
