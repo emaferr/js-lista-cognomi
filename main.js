51630c76df378f068d6ee81c8b5326dd15eec8f4
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-28 19:59:41 +0200

    Corretto formato nomi

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // chiedo all’utente il cognome
-// chiedo all’utente il cognome
+
 var cognomeUtente = prompt("Inserisci il tuo cognome");
-cognomeUtente = cognomeUtente.toUpperCase();
-console.log(cognomeUtente);
+cognomeUtente = cognomeUtente.toLowerCase().replace(/\b[a-z]/g, function(letter) { return letter.toUpperCase(); });
 
 // creo array 
 var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
@@ -13,9 +12,10 @@ listaCognomi.sort();
 var i = 0;
 
 while (i < listaCognomi.length){
-  var elemento = listaCognomi[i].toUpperCase();
+  var elemento = listaCognomi[i];
   var contenuto = document.getElementById("lista_cognomi").innerHTML;
   document.getElementById("lista_cognomi").innerHTML = contenuto + "<li>" + elemento + "</li>";
+
  
     i++;
 }
